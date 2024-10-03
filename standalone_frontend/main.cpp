0069e4e99dad9052ceb5e86bd771f967f724f7ca
Arves100 <4847635+arves100@users.noreply.github.com>
2024-10-03 21:47:49 +0200

    Always run mitigations at the start (I could have read the .h...)

--- standalone_frontend/main.cpp
+++ standalone_frontend/main.cpp
@@ -13,29 +13,16 @@ int main(int argc, char** argv)
     printf(u8"Revision %d (%s)\n", SERVER_REVISION, SERVER_COPYRIGHT);
     printf("Report any issues to %s\n\n", SERVER_ISSUE_URL);
 
-    bool runMigrations = false;
-
-    if (argc > 1)
-    {
-        if (strcmp(argv[1], "--run-migrations") == 0)
-            runMigrations = true;
-    }
-
     try
     {
         System::Instance().LoadSystemConfig("./gimuconfig.json");
 
-        if (runMigrations)
-        {
-            auto p = drogon::orm::DbClient::newSqlite3Client("filename=" + System::Instance().GetDbPath(), 1);
-            System::Instance().RunMigrations(p);
-            p->closeAll();
-            return 0;
-        }
-
         drogon::app()
             .loadConfigFile("./config.json")
             .createDbClient("sqlite3", "", 0, "", "", "", 1, System::Instance().GetDbPath(), "gme", false, "utf-8")
+            .registerBeginningAdvice([]() {
+            System::Instance().RunMigrations(drogon::app().getDbClient("gme"));
+            })
             .run()
         ;
     }
